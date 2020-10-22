72af1897de989c1352d1d0483952690174bd1843
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-22 11:47:28 -0300

    challenge de objetos de weatherstack

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('postman-request')
 
 const url = 'http://api.weatherstack.com/current?access_key=98260dd65c168d580fa241ee00d0e75e&query=-33.4985,-70.5756'
 
-request({ url: url }, (error, response) =>{
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) =>{
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature +' degress out. It feels like ' + response.body.current.feelslike + ' degress out.')
 })
