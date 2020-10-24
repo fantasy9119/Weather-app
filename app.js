44bde3416e8af1d269bd48f984dbd738b2758fd0
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-24 06:46:06 -0300

    Geocode callback abstraction y forecast callback abstraction challenge

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
 const url = 'http://api.weatherstack.com/current?access_key=98260dd65c168d580fa241ee00d0e75e&query=-33.4985,-70.5756'
 
@@ -13,18 +14,27 @@ const url = 'http://api.weatherstack.com/current?access_key=98260dd65c168d580fa2
 //     }
 // })
 
-//Geocoding
-// Address -> Lat/Long -> Weather
-const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2tpcmluZyIsImEiOiJja2dreTRrNXIwdjFhMnNwZXlqem11aTl5In0.ZJ5yV8SKrGeVNNytClya3Q&limit=1'
 
-request({url: url2, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service!')
-    } else if (response.body.features.length === 0) {
-        console.log('Invalid search')
-    } else {
-        console.log(response.body.features[0].center)
-    }
-    
-})
 
+
+
+// geocode('Santiago peñalolen la hijuela poniente 4196', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
