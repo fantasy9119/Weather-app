69643ad26221d06f1b23a69c9162d83f18c911c8
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-25 04:09:36 -0300

    ejemplo http completo

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -6,14 +6,20 @@ const request = http.request(url, (response) => {
     let data = ''
 
     response.on('data', (chunk) => {
-        console.log(chunk)
+        data = data + chunk.toString()
+        
 
     })
 
     response.on('end', () => {
-
+        const body = JSON.parse(data)
+        console.log(body)
     })
 
+})
+
+request.on('error', (error) => {
+    console.log('An error', error)
 })
 
 request.end()
