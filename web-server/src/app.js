b6bcfe675d946f06a8da96009f184c589eed2f16
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-29 13:55:10 -0300

    hbs basic setup

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,22 +5,13 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
 
-app.get('/help', (req,res) => {
-    res.send([{
-        name: 'Fabian',
-        age: 25
-    },{
-        name: 'ELWEONCITO',
-        age: 24
-    }])
+app.get('', (req,res)=>{
+    res.render('index')
 })
-
 
 app.get('/weather', (req,res) => {
     res.send({
