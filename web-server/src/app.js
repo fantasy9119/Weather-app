08b550d33fc8861d162c0d17bd1b7d6119dd9838
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-31 17:13:36 -0300

    setup básico listo para partials de handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 
 const app = express()
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -34,6 +37,7 @@ app.get('/about', (req,res) => {
 
 app.get('/help', (req,res) => {
     res.render('help',{
+        helpText: 'Some helpful text',
         title: 'Help me',
         name: 'Fabian'
     })
