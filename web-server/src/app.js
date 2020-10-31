25e7024527d8a93a41668de3fa297563abab3425
fantasy9119 <fabianmazzarellau@gmail.com>
2020-10-31 16:38:39 -0300

    customización directorio views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,14 +3,40 @@ const express = require('express')
 
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 
+
 app.get('', (req,res)=>{
-    res.render('index')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Fabian'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about',{
+        title: 'About me',
+        name: 'Fabian'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        title: 'Help me',
+        name: 'Fabian'
+    })
 })
 
 app.get('/weather', (req,res) => {
