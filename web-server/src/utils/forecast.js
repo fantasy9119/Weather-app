0cf5500afb568cd29ce3e4df2d033624e326ff2e
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-12 18:05:41 -0300

    Utils movido a src para modificar endpoint de weather, el cual quedo funcional con api de geocode y forecast

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request')
+
+const forecast = (latitude,longitude,callback) => {
+    
+    const url = 'http://api.weatherstack.com/current?access_key=98260dd65c168d580fa241ee00d0e75e&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, {body} = {}) =>{
+        if (error) {
+            callback('Unable to connect to weather service!',undefined)
+        }  else if (body.error) {
+            callback('Unable to find location\n'+ body.error.info, undefined)
+            
+        } else { 
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+        }
+    })
+}
+
+module.exports = forecast
