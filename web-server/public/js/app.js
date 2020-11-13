e101ba3dbd139acd0c314f96e8d2ee4aeddb2707
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-13 14:15:51 -0300

    Client side fetch del clima con input de html y muestra de resultados en la pagina

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,29 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
 
-fetch('http://localhost:3000/weather?address=la%20hijuela%20poniente%204196').then((response) => {
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    
+    messageOne.textContent = 'Loading Forecast...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+ location ).then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
+})
 })
