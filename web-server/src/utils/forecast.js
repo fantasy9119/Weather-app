c40963aa275351f0e66fe2573c8f3637e618b8b7
fantasy9119 <fabianmazzarellau@gmail.com>
2020-11-13 14:55:25 -0300

    degrees en vez de degress

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude,callback) => {
             callback('Unable to find location\n'+ body.error.info, undefined)
             
         } else { 
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
         }
     })
 }
